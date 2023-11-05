a907dc8ebf7486fbf20c4b7caf98b7a449e27e7b
Chris Couzens <ccouzens@gmail.com>
2023-11-05 13:38:32 +0000

    Update asahi-fedora.bash

--- asahi-fedora.bash
+++ asahi-fedora.bash
@@ -1,5 +1,5 @@
 # Make the keyboard work like a Dell keyboard
 sudo -s
-# echo 'options hid-apple swap_fn_leftctrl=1 swap_opt_cmd=1' > /etc/modprobe.d/keyboard.conf
+# echo 'options hid-apple swap_fn_leftctrl=1 swap_opt_cmd=1 iso_layout=1' > /etc/modprobe.d/keyboard.conf
 # dracut --regenerate-all --force
 
