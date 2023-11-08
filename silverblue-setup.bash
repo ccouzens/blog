3c5ac44ae00e256f342014afcc169ab76ecf6975
Chris Couzens <ccouzens@gmail.com>
2023-11-08 08:00:50 +0000

    Update silverblue-setup.bash
    Install nodejs for stuff

--- silverblue-setup.bash
+++ silverblue-setup.bash
@@ -55,7 +55,7 @@ toolbox run sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https:
 toolbox run dnf check-update
 toolbox run sudo dnf install code
 toolbox run sudo dnf copr enable varlad/helix
-toolbox run sudo dnf install helix wl-clipboard make clang gcc
+toolbox run sudo dnf install helix wl-clipboard make clang gcc nodejs-npm
 
 printf '#!/usr/bin/env bash\ntoolbox run /usr/bin/code --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland "$@"\n' > ~/.local/bin/code
 chmod +x ~/.local/bin/code
