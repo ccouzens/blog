75dff6f516c8d2b971f98ee4a713a41b11e79bdb
Chris Couzens <ccouzens@gmail.com>
2024-12-23 12:30:05 +0000

    Create chromebook-linux.bash

---
+++ chromebook-linux.bash
@@ -0,0 +1,15 @@
+sudo apt install wl-clipboard build-essential
+
+# set password
+sudo passwd chris
+
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > install.sh
+
+# unset password
+sudo passwd chris --delete
+
+brew install helix rustup
+rustup-init -y
+rustup component add rust-analyzer
+
+# other stuff from silverblue setup
