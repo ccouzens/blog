3d40f294bc63fd2dd7c58cf9d1503e0125c8b22e
Chris Couzens <ccouzens@gmail.com>
2026-04-25 16:21:52 +0100

    Create iot-setup.bash

---
+++ iot-setup.bash
@@ -0,0 +1,34 @@
+# Copy Fedora IOT to an SD card. Use a command like so
+sudo arm-image-installer \
+"--image=$HOME/Downloads/Fedora-IoT-raw-43-20251024.0.aarch64.raw.xz" \
+--target=rpi4 \
+--media=/dev/sda \
+--resizefs \
+--addkey "$HOME/.ssh/id_ed25519.pub" \
+--norootpass \
+--wifi-ssid=*** \
+--wifi-pass=***
+
+useradd chris -G wheel
+sudo passwd chris
+
+sudo rpm-ostree install \
+avahi \
+bat \
+clang \
+clang-tools-extra \
+cmake \
+cockpit \
+cockpit-machines \
+cockpit-ostree \
+cockpit-podman \
+helix \
+man-db \
+man-pages \
+ninja-build
+nodejs-npm \
+pnpm \
+ripgrep \
+rust-lldb \
+rustup \
+vim
