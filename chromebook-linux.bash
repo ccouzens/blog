b695760417ff84b1127a88cf997d4e3e8a58bd52
Chris Couzens <ccouzens@gmail.com>
2026-07-10 22:54:39 +0100

    Update chromebook-linux.bash

--- chromebook-linux.bash
+++ chromebook-linux.bash
@@ -1,13 +1,34 @@
 sudo apt install \
+adb \
+binaryen \
+bubblewrap \
 build-essential \
+clang \
+clang-tools \
+gcc \
 git \
 hx \
+make \
+man-db \
+nodejs \
+node-corepack \
 podman \
+ripgrep \
 rustup \
+wabt \
 wl-clipboard
 
 rustup-init -y
+rustup default stable
 rustup component add rust-analyzer
+
+# set password
+sudo passwd chris
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# unset password
+sudo passwd chris --delete
+
+brew install node pnpm
 
 # other stuff from silverblue setup
 
