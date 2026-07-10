d18d3fa082979f4ace29a4e408fe1f963bd80c8f
Chris Couzens <ccouzens@gmail.com>
2026-07-10 21:47:42 +0100

    Update chromebook-linux.bash

--- chromebook-linux.bash
+++ chromebook-linux.bash
@@ -1,14 +1,11 @@
-sudo apt install wl-clipboard build-essential
+sudo apt install \
+build-essential \
+git \
+hx \
+podman \
+rustup \
+wl-clipboard
 
-# set password
-sudo passwd chris
-
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > install.sh
-
-# unset password
-sudo passwd chris --delete
-
-brew install helix rustup
 rustup-init -y
 rustup component add rust-analyzer
 
